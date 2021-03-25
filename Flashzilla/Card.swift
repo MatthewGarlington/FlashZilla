b1a5c33b3ffe3c9fc35b7318c0c29b608e88de48
Matthew Garlington <SBMatthewG@gmail.com>
2021-03-25 10:51:52 -0400

    feat: Added Card Data Model
    The added Model holds the prompt and answer for each question

--- Flashzilla/Card.swift
+++ Flashzilla/Card.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+
+
+struct Card {
+    let prompt: String
+    var answer: String
+    
+    static var example: Card {
+        Card(prompt: "Who played the 13th Doctor in Doctor Who?", answer: "Jodie Whittaker")
+    }
+}
