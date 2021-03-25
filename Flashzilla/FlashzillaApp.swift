3f2fc35d2724e9cd09b0de7660bf77b5f93755b9
Matthew Garlington <SBMatthewG@gmail.com>
2021-03-25 09:12:26 -0400

    Initial Commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Matthew Garlington on 3/25/21.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
