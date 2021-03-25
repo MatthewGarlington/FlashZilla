ce138ae69218d739567c13d5edf52ba356fc16a4
Matthew Garlington <SBMatthewG@gmail.com>
2021-03-25 10:51:18 -0400

    Added Card
    The added card holds the prompt and answer upon tap gesture of the card. The Card styling is set to white with a shadow to overlay the border

--- Flashzilla/CardView.swift
+++ Flashzilla/CardView.swift
@@ -8,13 +8,37 @@
 import SwiftUI
 
 struct CardView: View {
+    let card: Card
+    @State private var isShowingAnswer = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            RoundedRectangle(cornerRadius: 25.0, style: .continuous)
+                .fill(Color.white)
+                .shadow(radius: 10)
+            
+            VStack {
+                Text(card.prompt)
+                    .font(.largeTitle)
+                    .foregroundColor(.black)
+                if isShowingAnswer {
+                Text(card.answer)
+                    .font(.title)
+                    .foregroundColor(.gray)
+                }
+            }
+            .padding(20)
+            .multilineTextAlignment(.center)
+        }
+        .frame(width: 450, height: 250)
+        .onTapGesture {
+            self.isShowingAnswer.toggle()
+        }
     }
 }
 
 struct CardView_Previews: PreviewProvider {
     static var previews: some View {
-        CardView()
+        CardView(card: Card.example)
     }
 }
