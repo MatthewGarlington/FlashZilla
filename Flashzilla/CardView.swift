03d84e4351fe1a488e23b71456dcabcbc5eac303
Matthew Garlington <SBMatthewG@gmail.com>
2021-03-25 12:11:12 -0400

    feat: Added A swipe gesture animation
    This gesture was added to allow the user to swipe away each card to remove it from the stack and show the next card

--- Flashzilla/CardView.swift
+++ Flashzilla/CardView.swift
@@ -9,9 +9,13 @@ import SwiftUI
 
 struct CardView: View {
     let card: Card
+    
     @State private var isShowingAnswer = false
+    @State private var offset = CGSize.zero
+    var removal: (() -> Void)? = nil
     
     var body: some View {
+       
         ZStack {
             RoundedRectangle(cornerRadius: 25.0, style: .continuous)
                 .fill(Color.white)
@@ -31,6 +35,22 @@ struct CardView: View {
             .multilineTextAlignment(.center)
         }
         .frame(width: 450, height: 250)
+        .rotationEffect(.degrees(Double(offset.width / 5)))
+        .offset(x: offset.width * 5, y: 0)
+        .opacity(2 - Double(abs(offset.width / 50 )))
+        .gesture(
+            DragGesture()
+                .onChanged { gesture in
+                    self.offset = gesture.translation
+                }
+                .onEnded { _ in
+                    if abs(self.offset.width) > 100 {
+                        self.removal?()
+                    } else {
+                        self.offset = .zero
+                    }
+                }
+        )
         .onTapGesture {
             self.isShowingAnswer.toggle()
         }
